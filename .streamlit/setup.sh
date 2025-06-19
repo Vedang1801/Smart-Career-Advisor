2048d85c4aafb6a498641a4bf24dcfb712190226
Vedang Malusare <vedangmalusare1807@gmail.com>
2025-06-18 23:25:06 -0400

    fix: update .streamlit/setup.sh for robust spaCy model install

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+echo "Checking for spaCy installation..."
+if ! python3 -c "import spacy" &> /dev/null; then
+    echo "spaCy not found. Installing spaCy..."
+    pip install spacy
+fi
+
 echo "Installing spaCy model..."
 
 python3 -m spacy download en_core_web_sm
